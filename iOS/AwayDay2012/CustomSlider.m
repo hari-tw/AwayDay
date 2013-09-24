75f1d8f1b8dd31436a25a99db38ef7e8a8d6cdbf
Mohammad Safad <safadmoh@thoughtworks.com>
2013-09-24 17:34:34 +0530

    share button removed from slider menu

--- iOS/AwayDay2012/CustomSlider.m
+++ iOS/AwayDay2012/CustomSlider.m
@@ -38,7 +38,7 @@
                         [UIImage imageNamed:@"my_schedule"],
                          [UIImage imageNamed:@"gallery"],
                         [UIImage imageNamed:@"route-map"],
-                        [UIImage imageNamed:@"share2"],
+                       // [UIImage imageNamed:@"share2"],
                         
                         ];
     NSArray *colors = @[
@@ -50,7 +50,7 @@
                         [UIColor colorWithRed:240/255.f green:159/255.f blue:254/255.f alpha:1],
                         [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
                         
-                        [UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
+                        //[UIColor colorWithRed:119/255.f green:152/255.f blue:255/255.f alpha:1],
                         
                         
                         
